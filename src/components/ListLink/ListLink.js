c75e59a6109258a3890729741ca8f44dc87898b9
maslana.m <maslana.m89@gmail.com>
2021-01-23 08:09:50 +0100

    Update files

--- src/components/ListLink/ListLink.js
+++ src/components/ListLink/ListLink.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import styles from './ListLink.scss';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
-import {settings} from '../../data/dataStore';
+import { Link } from 'react-router-dom';
+import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 
-const ListLink = ({id, title, image, description}) => (
+const ListLink = ({ id, title, image, description }) => (
   <Link to={`/list/${id}`} className={styles.link}>
     <section className={styles.component}>
       <div className={styles.text}>
         <h2 className={styles.title}>{ReactHtmlParser(title)}</h2>
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
+        <div className={styles.description}>{ReactHtmlParser(description)}</div>
       </div>
       <div className={styles.image}>
         <img src={image} />
@@ -25,7 +23,7 @@ ListLink.propTypes = {
   id: PropTypes.string,
   title: PropTypes.node.isRequired,
   description: PropTypes.node,
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
 };
 
 ListLink.defaultProps = {
